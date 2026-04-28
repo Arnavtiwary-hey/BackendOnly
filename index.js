d06c19d4782fdfce4b2a8de867ea22d8a7968b41
Arnav tiwary <109584609+Arnavtiwary-hey@users.noreply.github.com>
2026-04-28 22:40:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=> {
+    res.send('arnav.com')
+})
+app.get('/login',(req,res) =>{
+     res.send('<h1>please login at arnav tiwary</h1>')
+})
+app.get('/youtube', (req,res) =>{
+  res.send('<h2>parasar yrs</h2>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
